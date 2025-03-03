f7cc34b1197c37f2fc1481c4c6ba67e2a15ea29a
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-03-03 16:46:29 +0700

    add task14

---
+++ task14.c
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+#include <stddef.h>
+
+struct ListNode {
+  int val;
+  struct ListNode* next;
+};
+
+typedef struct ListNode ListNode;
+
+ListNode* detectCycle(ListNode* head) {
+  ListNode* slow = head;
+  ListNode* fast = head;
+
+  while (fast != NULL && fast->next != NULL) {
+    slow = slow->next;
+    fast = fast->next->next;
+
+    if (slow == fast) {
+      slow = head;
+      while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+      }
+
+      return slow;
+    }
+  }
+
+  return NULL;
+}
