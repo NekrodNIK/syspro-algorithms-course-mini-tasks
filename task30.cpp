2d6393a52f0c0b57816174a2842ef03c015bc345
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-01 18:50:52 +0700

    add task30

--- task30.cpp
+++ task30.cpp
@@ -3,6 +3,14 @@
 class Solution {
 public:
     int maxProfit(std::vector<int>& prices) {
+        int money = 0;
         
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i-1]) {
+                money += prices[i] - prices[i-1];
+            }
+        }   
+
+        return money;
     }
 };
