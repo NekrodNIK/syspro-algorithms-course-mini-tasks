31f670c35847dca8b2bf3991e1cf49b952716de4
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-01 18:36:29 +0700

    add task29

--- task29.cpp
+++ task29.cpp
@@ -1,7 +1,18 @@
+#include <algorithm>
 #include <vector>
 
 class Solution {
 public:
     bool canJump(std::vector<int>& nums) {
+        int max;
+        for (int i; i < nums.size(); i++) {
+            if (i > max) {
+                return false;
+            }
+
+            max = std::max(max, i + nums[i]);
+        }
+
+        return true;
     }
 };
