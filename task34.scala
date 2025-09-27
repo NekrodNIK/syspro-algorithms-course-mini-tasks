1974ef366cf0363f60043fadb6ce61315d733624
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-09-27 16:30:43 +0700

    add task34

---
+++ task34.scala
@@ -0,0 +1,8 @@
+object Solution:
+    def numTrees(n: Int): Int = 
+        val arr = Array.fill(n + 1)(0)
+        arr(0) = 1
+
+        for (i <- 1 to n; j <- 1 to i)
+            arr(i) += arr(j-1) * arr(i-j)    
+        arr(n)
