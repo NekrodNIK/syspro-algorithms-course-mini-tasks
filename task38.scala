4c195886d03f04b9eab5c51bed0f9fcde465f478
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-10-27 13:40:02 +0700

    fix

--- task38.scala
+++ task38.scala
@@ -1,35 +1,35 @@
 object Solution:
-  def findTheCity(n: Int, edges: Array[Array[Int]], distanceThreshold: Int): Int = 
+  def findTheCity(n: Int, edges: Array[Array[Int]], distanceThreshold: Int): Int =
     var B = Array.fill(n, n)(Int.MaxValue)
 
-    for (i <- 0 until n)
-      B(i)(i) = 0
+    for i <- 0 until n
+    do B(i)(i) = 0
 
-    for ((from, to, w) <- edges.iterator.map((arr) => (arr(0), arr(1), arr(2))))
+    for (from, to, w) <- edges.iterator.map((arr) => (arr(0), arr(1), arr(2)))
+    do
       B(from)(to) = w
       B(to)(from) = w
-    
-    for {
+
+    for 
       k <- 0 until n
       i <- 0 until n
       j <- 0 until n
       if (B(i)(k) != Int.MaxValue && B(k)(j) != Int.MaxValue)
-    } B(i)(j) = Math.min(B(i)(j), B(i)(k) + B(k)(j))
+    do B(i)(j) = Math.min(B(i)(j), B(i)(k) + B(k)(j))
 
     var ansInd = Int.MaxValue
     var ansCnt = Int.MaxValue
 
-    for (i <- 0 until n) {
+    for i <- 0 until n
+    do
       var cnt = 0
-      for {
+      for
         j <- 0 until n
         if (i != j && B(i)(j) <= distanceThreshold)
-      } cnt += 1
-      
+      do cnt += 1
+
       if (cnt <= ansCnt)
         ansInd = i
         ansCnt = cnt
-    }
 
-    ansInd   
-
+    ansInd
