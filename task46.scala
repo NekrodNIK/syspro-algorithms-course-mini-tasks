6ccea332ffc093eae0ff316a5b829f8d271e43b3
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-12-15 15:15:27 +0700

    fix task46

--- task46.scala
+++ task46.scala
@@ -1,13 +1,15 @@
 package task46
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
-case class Node(
-    parent: Node,
-    charToParent: Char,
+class Node(
+    val parent: Node,
+    val charToParent: Char,
     var suffLink: Node = null,
-    var terminal: Boolean = false,
-    go: mutable.Map[Char, Node] = mutable.Map.empty
+    var terminal: String = null,
+    val go: mutable.Map[Char, Node] = mutable.Map.empty,
+    var count: Int = 0
 )
 
 case class FilterResult(
@@ -15,14 +17,8 @@ case class FilterResult(
     total: Int
 )
 
-
-class SpamFilter {
-  private val root = Node(null, ' ')
-  private case class MapValue(
-    var node: Node,
-    var counter: Int
-  )
-  private val triggerMap = mutable.Map[String, MapValue]()
+class SpamFilter(triggers: mutable.ArrayBuffer[String]) {
+  private var root: Node = Node(null, ' ')
 
   private def suf(v: Node): Node = {
     if v.suffLink == null then
@@ -40,46 +36,86 @@ class SpamFilter {
     v.go(c)
   }
 
+  private def rebuild: Unit = {
+    root = Node(null, ' ')
+    for trigger <- triggers do      
+      var v = root
+      for c <- trigger do
+        v = v.go.getOrElseUpdate(c, Node(v, c))
+      v.terminal = trigger
+  }
+
   def addTrigger(trigger: String): Unit = {
-    val v = trigger.foldRight(root)((c, v) => v.go.getOrElseUpdate(c, Node(v, c)))
-    v.terminal = true
-    triggerMap(trigger) = MapValue(v, 0)
+    triggers += trigger
+    rebuild
   }
-  def addTriggers(triggers: Iterable[String]): Unit = triggers.foreach(addTrigger(_))
 
-  def apply(str: String): FilterResult = {
-    triggerMap.keys.foreach(triggerMap(_).counter = 0)
+  def apply(str: String): FilterResult = {  
+    val map   = mutable.HashMap[String, Int]().withDefaultValue(0)
+    var total = 0
 
-    var v = root
-    for i <- 0 until str.length do
-      v = next(v, str(i))
+    var cur = root
 
-      var temp = v
-      while temp != root do
-        if temp.terminal then
-          triggerMap.find { case (trigger, _) => temp == triggerMap(trigger)._1 } match {
-            case Some((trigger, _)) => triggerMap(trigger).counter += 1
-            case None               =>
-          }
-        temp = suf(temp)
+    val visited = mutable.HashSet[Node]()
 
-    val total = triggerMap.values.map(_.counter).sum
-    FilterResult(triggerMap.mapValues(_.counter).toMap, total)
+    for s <- str do
+      cur = next(cur, s)
+
+      if !visited.contains(cur) then
+        cur.count = 0
+        visited += cur
+      cur.count += 1
+
+      var suf_node = cur
+      while suf_node != root do
+        val trigger = suf_node.terminal
+        if trigger != null then
+          map(trigger) += cur.count
+          total += 1
+        suf_node = suf(suf_node)
+
+    FilterResult(map.toMap, total)
   }
+
 }
 
 object SpamFilter {
   def apply(initial_triggers: Iterable[String]): SpamFilter = {
-    val obj = new SpamFilter
-    obj.addTriggers(initial_triggers)
+    val obj = new SpamFilter(ArrayBuffer.from(initial_triggers))
+    obj.rebuild
     obj
   }
 }
 
 @main def main: Unit = {
-  val spam_filter = SpamFilter(Seq("buy", "cat", "octocat", "toc", "octo", "crypto", "binance", "easy money"))
-  val result      = spam_filter("crypto buy now easy money buybinancecryptooctocatbuy")
+  val spam_filter = SpamFilter(
+    Seq(
+      "buy",
+      "easy money",
+      "spam",
+      "buy now",
+      "free money",
+      "click now",
+      "click",
+      "money",
+      "binance",
+      "bitcoin",
+      "ton",
+      "now"
+    )
+  )
+  val str = "easy money making with cryptocurrency (ton, bitcoin, ethereum) - click now and start making money"
 
+  var result = spam_filter(str)
+  println(s"Triggers: ${result.triggers}")
+  println(s"Total: ${result.total}")
+
+  spam_filter.addTrigger("ethereum")
+  result = spam_filter(str)
+  println(s"Triggers: ${result.triggers}")
+  println(s"Total: ${result.total}")
+
+  result = spam_filter("easy money")
   println(s"Triggers: ${result.triggers}")
   println(s"Total: ${result.total}")
 }
